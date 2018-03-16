b71187a52e92174ddf4248adbc6ed94be14c8be1
Radu Dinu <radurmx@gmail.com>
2018-03-16 08:55:15 +0200

    Update Consumer.java

--- Consumer.java
+++ Consumer.java
@@ -6,9 +6,9 @@ public class Consumer extends Thread {
 
 	public Stack stack;
 	private Semaphore semFull;
-	private Semaphore semFree; 
-	
-	public Consumer(Stack stack1, Semaphore semFull, Semaphore semFree) {
+	private Semaphore semFree;
+
+	public Consumer(Stack stack1, Semaphore semFree, Semaphore semFull) {
 		super();
 		this.stack = stack1;
 		this.semFree = semFree;
@@ -22,17 +22,20 @@ public class Consumer extends Thread {
 			
 			try {
 				semFull.acquire();
+				System.out.println("1");
 			} catch (InterruptedException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
+
 			synchronized (stack) {
-	
+				System.out.println("2");
 				stack.pop();
+				System.out.println("3");
 			}
-			
+			System.out.println("4");
 			semFree.release();
+			System.out.println("5");
 			
 			try {
 				sleep(6000);
@@ -44,10 +47,8 @@ public class Consumer extends Thread {
 			System.out.print("Element removed --- ");
 			stack.printEmptySlotsAndElement();
 			System.out.println(" removed");
-			//stack.pop();
-				
-			}
-		}
-	
+			// stack.pop();
 
+		}
+	}
 }
